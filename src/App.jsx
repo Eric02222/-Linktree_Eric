7e9d27949f64b7173253d90a08e636960b0af435
Eric <ericmaraoliveira@gmail.com>
2025-04-02 22:09:45 -0300

    adição link linkedin

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
 import { FaGithub } from "react-icons/fa";
+import Linkedin from './components/Linkedin';
 
 function App() {
 
   return (
     <div className='container-app'>
       <h1>Links Eric Mara</h1>
-
       
-      <a href="https://github.com/Eric02222" className='link-git' target='_blank'><FaGithub />Github</a>
+      <a href="https://github.com/Eric02222" className='links' target='_blank'><FaGithub />Github</a>
+
+      <Linkedin/>
 
     </div>
   )
---
+++ src/components/Linkedin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaLinkedin } from "react-icons/fa";
+import './Linkedin.css'
+
+function Linkedin() {
+  return (
+
+      <a href="https://github.com/Eric02222" className='link-linkedin links ' target='_blank'><FaLinkedin />Linkedin</a>
+      
+  )
+}
+
+export default Linkedin
