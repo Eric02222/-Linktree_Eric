8f2c56eaf2466bd25f3fcd4a648f02acdcbdfbc8
Eric <ericmaraoliveira@gmail.com>
2025-04-02 21:16:27 -0300

    Linktree com link da minha conta do github

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { FaGithub } from "react-icons/fa";
+
+function App() {
+
+  return (
+    <div className='container-app'>
+      <h1>Links Eric Mara</h1>
+
+      
+      <a href="https://github.com/Eric02222" className='link-git' target='_blank'><FaGithub />Github</a>
+
+    </div>
+  )
+}
+
+export default App
